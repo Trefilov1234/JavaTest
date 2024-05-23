e0efe6956275cb2e317ded3ee9768081ed07e3b3
exfadra <trefilov.2002@inbox.ru>
2024-05-23 18:01:47 +0400

    First commit

---
+++ Proj_1/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        //System.out.println("“Your time is limited,\n    so don’t waste it\n       living someone else’s life”\n              Steve Jobs");
+        //task2
+        /*Scanner in = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Input second number: ");
+        int num2 = in.nextInt();
+        System.out.printf("first number: %d \n",num1);
+        System.out.printf("second number: %d \n",num2);
+        double res=num2/100f*num1;
+        System.out.printf("result: %.2f %% \n",res);
+        in.close();*/
+        //task3
+        /*Scanner in = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Input second number: ");
+        int num2 = in.nextInt();
+        System.out.print("Input third number: ");
+        int num3 = in.nextInt();
+        String res=new String(Integer.toString(num1)+Integer.toString(num2)+Integer.toString(num3));
+        System.out.printf("result: %s \n",res);
+        in.close();*/
+        //task4
+        /*Scanner in = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int num1 = in.nextInt();
+        String str=Integer.toString(num1);
+        boolean flag=true;
+        while(flag) {
+            if (str.length() < 6) {
+                System.out.print("error\n");
+                System.out.print("Input number: ");
+                num1 = in.nextInt();
+                str=Integer.toString(num1);
+                flag=true;
+            }
+            else
+                flag=false;
+        }
+        String newStr=Character.toString(str.charAt(5))+str.charAt(4)+str.charAt(2)+str.charAt(3)+str.charAt(1)+str.charAt(0);
+        System.out.printf("result: %s \n",newStr);*/
+        //task5
+
+    }
+}
